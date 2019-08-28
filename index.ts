741f61eb50ce6cbc28a9ebb38632e2718d025792
HamdiAlaa* <alaahamdi57@gmail.com>
2019-08-28 12:21:32 +0100

    azure-aws files added

--- index.ts
+++ index.ts
@@ -1,83 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-// import {Infra_config_params} from "./Infra_config_params" ;
-
+import * as az from './azureVM';
 let _config = require('../config/_az_vm.json');
-//Create an Azure Resource Group
-const resourceGroup = new azure.core.ResourceGroup("resourceGroup", {
-    location: _config.location,
-});
+let main_config = require('../config/main_config.json');
 
 
-//NetWork
-const mainVirtualNetwork = new azure.network.VirtualNetwork("main",{
-    addressSpaces :["10.0.0.0/16"],
-    location: resourceGroup.location,
-    name: `${_config.prefix}-network`,
-    resourceGroupName: resourceGroup.name,
-});
-
-//Subnet
-const internal = new azure.network.Subnet("internal",{
-    addressPrefix:"10.0.2.0/24",
-    name:"internal",
-    resourceGroupName:resourceGroup.name,
-    virtualNetworkName:mainVirtualNetwork.name
-
-});
-
-// main interface
-for (let index = 1; index <= _config.vmNumber; index++) {
-const mainNetworkInterface = new azure.network.NetworkInterface(`main${index}`, {
-    ipConfigurations: [{
-        name: `testconfiguration${index}`,
-        privateIpAddressAllocation: "Dynamic",
-        subnetId: internal.id,
-    }],
-    location: resourceGroup.location,
-    name: `${_config.prefix}-nic-${index}`,
-    resourceGroupName: resourceGroup.name,
-});
-
-//Create the virtual machine 
-
-const mainVirtualMachine = new azure.compute.VirtualMachine(`VM-${index}`, {
-    location: resourceGroup.location,
-    name: `${_config.prefix}-vm-${index}`,
-    networkInterfaceIds: [mainNetworkInterface.id],
-    osProfile: {
-        adminPassword: _config.password,
-        adminUsername: _config.username,
-        computerName: `hostname${index}`,
-    },
-    osProfileLinuxConfig: {
-        disablePasswordAuthentication: false,
-    },
-    resourceGroupName: resourceGroup.name,
-    storageImageReference: {
-        offer: _config.offer,
-        publisher: "Canonical",
-        sku: _config.sku,
-        version: _config.version,
-    },
-    storageOsDisk: {
-        caching: "ReadWrite",
-        createOption: "FromImage",
-        managedDiskType: "Standard_LRS",
-        name: `mytestosdisk${index}`,
-    },
-    tags: {
-        environment: "staging",
-    },
-    vmSize: _config.vmsize,
-});
+/************ if it is azure infra********************/
+if(main_config.isAzure && !main_config.isAws){
+    const az_infra = new az.Vms();
 }
-//Create an Azure resource (Storage Account)
-const account = new azure.storage.Account("storage", {
-    resourceGroupName: resourceGroup.name,
-    accountTier: "Standard",
-    accountReplicationType: "LRS",
-});
-// Export the connection string for the storage account
-export const connectionString = account.primaryConnectionString;
-
